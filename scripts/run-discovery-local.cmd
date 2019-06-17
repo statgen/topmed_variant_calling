bc22c04537680ae46a785f0a2c3fbb321ad91cce
Jonathon LeFaive <jonathonl@users.noreply.github.com>
2019-06-17 14:50:53 -0400

    Fixes mkdir call in run-discovery-local.cmd

--- scripts/run-discovery-local.cmd
+++ scripts/run-discovery-local.cmd
@@ -3,7 +3,7 @@
 # var : ROOT : ..
 # target : out/sm/$SMIDX$1$/$SMIDX$1$.vb2 out/sm/$SMIDX$1$/$SMIDX$1$.norm.xy out/sm/$SMIDX$1$/$SMIDX$1$.bcf out/sm/$SMIDX$1$/$SMIDX$1$.bcf.csi
 # name: example-discovery
-mkdir --p out/sm/$SMIDX$1$/
+mkdir -p out/sm/$SMIDX$1$/
 bash -c 'set -o pipefail; REF_PATH=resources/ref/md5/%2s/%2s/%s $ROOT$/samtools/samtools view -uh -T resources/ref/hs38DH.fa $SMIDX$2$ 2> out/sm/$SMIDX$1$/$SMIDX$1$.bcf.samtools_err | $ROOT$/bamUtil/bin/bam clipoverlap --poolSize 100000000 --in -.ubam --out -.ubam 2> out/sm/$SMIDX$1$/$SMIDX$1$.bcf.bamUtil_err | $ROOT$/vt-topmed/vt discover2 -z -q 20 -b + -r resources/ref/hs38DH.fa -s $SMIDX$1$ -o out/sm/$SMIDX$1$/$SMIDX$1$.bcf 2> out/sm/$SMIDX$1$/$SMIDX$1$.bcf.vt_err'
 $ROOT$/bcftools/bcftools index -f out/sm/$SMIDX$1$/$SMIDX$1$.bcf
 REF_PATH=resources/ref/md5/%2s/%2s/%s $ROOT$/cramore/cramore cram-verify-bam --svd resources/ref/HGDP_938.b38.genotypes.svd --sam $SMIDX$2$ --cap-DP 100 --out out/sm/$SMIDX$1$/$SMIDX$1$.vb2 --num-PC 4 > out/sm/$SMIDX$1$/$SMIDX$1$.vb2.stdout 2> out/sm/$SMIDX$1$/$SMIDX$1$.vb2.stderr
